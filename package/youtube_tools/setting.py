b4942e2e420dab5ab9f1b2eb80e5175a9238d08f
pthnhan <nhanmath97@gmail.com>
2021-10-31 03:23:13 +0700

    package youtube_tools

--- package/youtube_tools/setting.py
+++ package/youtube_tools/setting.py
@@ -7,8 +7,10 @@ load_dotenv(verbose=True, override=True)
 
 # OR, explicitly providing path to '.env'
 from pathlib import Path  # Python 3.6+ only
-env_path = Path('/home/pthnhan/Desktop/other/khtn_ktxldl/example/envfile') / '.env'
-load_dotenv(dotenv_path=env_path)
-print(env_path)
-# load_dotenv(find_dotenv())
-print(find_dotenv())
+if len(find_dotenv()) > 0:
+    load_dotenv(find_dotenv())
+    print(find_dotenv())
+else:
+    env_path = Path('/home/pthnhan/Desktop/other/khtn_ktxldl/example/envfile') / '.env'
+    load_dotenv(dotenv_path=env_path)
+    print(env_path)
